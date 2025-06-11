6d4be1317c5236dac0baed5f61b419e63b6a6cdb
Johng117 <johngorman117@gmail.com>
2025-06-11 21:37:51 +0100

    completion of desktop view, including active states

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // element variables
 const shareButton = document.getElementById("avatar-icon-share-container");
+const desktopShareButton = document.getElementById("avatar-icon-share-container-desktop")
 const avatarInfoGroup = document.getElementById("avatar-info-container");
 const socialGroup = document.getElementById("social-icons");
 const avatarSocialContainer = document.getElementById(
   "avatar-social-container"
 );
 const shareImage = document.getElementById("avatar-icon-share");
+const desktopShareImage = document.getElementById("avatar-icon-share-desktop");
 const svgBox = document.getElementById("svg-container");
 
 // toggle function to switch views of avatar/social media element
 function toggleFunc() {
-  if (window.innerWidth < 1044) {
+  
     if (window.getComputedStyle(avatarInfoGroup).display === "flex") {
       avatarInfoGroup.style.display = "none";
       socialGroup.style.display = "flex";
@@ -24,19 +26,28 @@ function toggleFunc() {
       shareButton.style.backgroundColor = "#ecf2f8";
       shareImage.style.cssText = "filter: brightness(100%)";
     }
-  } else if(window.innerWidth >=1044) {
-    
+  }
+
+  function desktopToggleFunc() {
     if(window.getComputedStyle(svgBox).visibility === "hidden") {
       svgBox.style.visibility = "visible";
+      desktopShareButton.style.backgroundColor = "#48556a";
+      desktopShareImage.style.cssText = "filter: brightness(250%)"
     } else {
       svgBox.style.visibility = "hidden";
+      desktopShareButton.style.backgroundColor = "#ecf2f8";
+      desktopShareImage.style.cssText = "filter: brightness(100%)"
     }
-
   }
-}
+  
+    
+    
+
+  
+
 
 shareButton.addEventListener("click", toggleFunc);
-
+desktopShareButton.addEventListener("click", desktopToggleFunc);
 // if (window.innerWidth >= 1044) {
 //   console.log("bigger than  1044");
 //   toolTip.style.visibility = "visible";
