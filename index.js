34eef3ea0c1a1de84e6d227a4ebeb347d4f66802
Johng117 <johngorman117@gmail.com>
2025-06-11 21:58:37 +0100

    added README.md, deletions and cleanup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // element variables
 const shareButton = document.getElementById("avatar-icon-share-container");
-const desktopShareButton = document.getElementById("avatar-icon-share-container-desktop")
+const desktopShareButton = document.getElementById(
+  "avatar-icon-share-container-desktop"
+);
 const avatarInfoGroup = document.getElementById("avatar-info-container");
 const socialGroup = document.getElementById("social-icons");
 const avatarSocialContainer = document.getElementById(
@@ -10,46 +12,35 @@ const shareImage = document.getElementById("avatar-icon-share");
 const desktopShareImage = document.getElementById("avatar-icon-share-desktop");
 const svgBox = document.getElementById("svg-container");
 
-// toggle function to switch views of avatar/social media element
+// toggle functions to switch views of avatar/social media element
 function toggleFunc() {
-  
-    if (window.getComputedStyle(avatarInfoGroup).display === "flex") {
-      avatarInfoGroup.style.display = "none";
-      socialGroup.style.display = "flex";
-      avatarSocialContainer.style.backgroundColor = "#48556a";
-      shareButton.style.backgroundColor = "#9eafc2";
-      shareImage.style.cssText = "filter: brightness(250%)";
-    } else {
-      avatarInfoGroup.style.display = "flex";
-      socialGroup.style.display = "none";
-      avatarSocialContainer.style.backgroundColor = "#ffffff";
-      shareButton.style.backgroundColor = "#ecf2f8";
-      shareImage.style.cssText = "filter: brightness(100%)";
-    }
+  if (window.getComputedStyle(avatarInfoGroup).display === "flex") {
+    avatarInfoGroup.style.display = "none";
+    socialGroup.style.display = "flex";
+    avatarSocialContainer.style.backgroundColor = "#48556a";
+    shareButton.style.backgroundColor = "#9eafc2";
+    shareImage.style.cssText = "filter: brightness(250%)";
+  } else {
+    avatarInfoGroup.style.display = "flex";
+    socialGroup.style.display = "none";
+    avatarSocialContainer.style.backgroundColor = "#ffffff";
+    shareButton.style.backgroundColor = "#ecf2f8";
+    shareImage.style.cssText = "filter: brightness(100%)";
   }
+}
 
-  function desktopToggleFunc() {
-    if(window.getComputedStyle(svgBox).visibility === "hidden") {
-      svgBox.style.visibility = "visible";
-      desktopShareButton.style.backgroundColor = "#48556a";
-      desktopShareImage.style.cssText = "filter: brightness(250%)"
-    } else {
-      svgBox.style.visibility = "hidden";
-      desktopShareButton.style.backgroundColor = "#ecf2f8";
-      desktopShareImage.style.cssText = "filter: brightness(100%)"
-    }
+function desktopToggleFunc() {
+  if (window.getComputedStyle(svgBox).visibility === "hidden") {
+    svgBox.style.visibility = "visible";
+    desktopShareButton.style.backgroundColor = "#48556a";
+    desktopShareImage.style.cssText = "filter: brightness(250%)";
+  } else {
+    svgBox.style.visibility = "hidden";
+    desktopShareButton.style.backgroundColor = "#ecf2f8";
+    desktopShareImage.style.cssText = "filter: brightness(100%)";
   }
-  
-    
-    
-
-  
-
+}
 
 shareButton.addEventListener("click", toggleFunc);
 desktopShareButton.addEventListener("click", desktopToggleFunc);
-// if (window.innerWidth >= 1044) {
-//   console.log("bigger than  1044");
-//   toolTip.style.visibility = "visible";
-//   return;
-// }
+
